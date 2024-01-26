f3878b39ddca1a91a599d3901bbe8e973c47cb6c
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2024-01-26 11:37:40 +0530

    qustions should not contain answers

--- questions/q2/q2.go
+++ questions/q2/q2.go
@@ -5,26 +5,9 @@
 */
 
 package q2
-import "fmt"
- 
-var factVal int = 1 
-var i int = 1
-var n int
  
 // Fact function, please do NOT change it's name :)
 func Fact(num int) int {
 	// Your code goes here
-	if(n >0){        
-        for i:=1; i<=n; i++ {
-            factVal *= int(i)  
-        }
-         
-    }    
-    return factVal  
-}
-
-func main(){    
-    fmt.Scan(&n)   
-    fmt.Print("Factorial is: ",Fact(n))
 }
 
